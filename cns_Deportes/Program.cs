a1fcd8c410e972ebea725a05e89eb31b96f6c81b
Sala K 301 <salak301@salasfr.itm.edu.co>
2025-08-28 09:46:39 -0500

    Primer Commit

--- cns_Deportes/Program.cs
+++ cns_Deportes/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+public class Uniformes{
+    public String? Nombre_Equipo;
+    public String Color;
+    public int NumeroCamiseta;
+    public int TallaMedias;
+    public int TallaGuayos;
+}
+public class Balones{
+    public String Marca;
+    public String Color;
+    public decimal Precio;
+}
+public class Cono{
+    public int ID_cono;
+    public decimal Altura;
+    public bool Funcionan;
+}
