15f6967bfd009e329b9b6c3d405265f74563b3f4
Juan Pablo Zuluaga <juanpablozuluagasalazar6@gmail.com>
2025-08-28 09:53:49 -0500

    Primer commit

--- cns_Deportes/Program.cs
+++ cns_Deportes/Program.cs
@@ -1,8 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
 
+Uniformes persona = new Uniformes();
+persona.id=01;
+persona.Nombre_Equipo="Depotes fc ";
+persona.Color="Verde";
+persona.NumeroCamiseta=10;
+persona.TallaMedias=5;
+persona.TallaGuayos=35;
+
 public class Uniformes{
+    public int id;
     public String? Nombre_Equipo;
     public String Color;
     public int NumeroCamiseta;
@@ -18,4 +27,10 @@ public class Cono{
     public int ID_cono;
     public decimal Altura;
     public bool Funcionan;
+}
+
+public class Deportes{
+    public int ID;
+    public DateTime FechaTorneo;
+    public String TipoBalon;
 }
